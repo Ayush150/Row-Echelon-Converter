ca65c10e0facf9fb3ce6ba60a45ef094a80c935a
Ayush Jain <ayush.aj2006@gmail.com>
2025-12-05 23:38:12 +0530

    Made the base of project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int order_of_matrix;
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &order_of_matrix);
+    vector<vector<float>> matrix(order_of_matrix, vector<float>(order_of_matrix));
+    printf("Enter the elements of the matrix row-wise:\n");
+    for (int i = 0; i < order_of_matrix; i++) {
+        for (int j = 0; j < order_of_matrix; j++) {
+            scanf("%f", &matrix[i][j]);
+        }
+    }
+    printf("The entered matrix is:\n");
+    for (int i = 0; i < order_of_matrix; i++) {
+        for (int j = 0; j < order_of_matrix; j++) {
+            printf("%.2f ", matrix[i][j]);
+        }
+        printf("\n");
+    }   
+}
