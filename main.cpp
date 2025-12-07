b4bac502bc8152c87538918e31308a9fc36e8037
Ayush Jain <ayush.aj2006@gmail.com>
2025-12-07 15:47:20 +0530

    Made a header file to define funtions

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main() {
             scanf("%f", &matrix[i][j]);
         }
     }
+    
     printf("The entered matrix is:\n");
     for (int i = 0; i < order_of_matrix; i++) {
         for (int j = 0; j < order_of_matrix; j++) {
